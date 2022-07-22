5b0bb5d398ef6bf44c11a3b7ced5e081ab688dc5
termidn <108564427+termidn@users.noreply.github.com>
2022-07-22 09:30:43 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -47,10 +47,9 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
     buttons.buildbutton("Repo", "https://github.com/Dawn-India/Z-Mirror")
-    buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
-    buttons.buildbutton("Owner", "https://t.me/z_mirror")
+    buttons.buildbutton("Mirror Group", "https://t.me/+EekcqYPH5NQ1Zjhl")
+    buttons.buildbutton("Owner", "https://t.me/Terminator090")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
